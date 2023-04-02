b945f052f8d4048fa62cee08f3fbfdee743cba42
animal-cientifico <leo.ff.xd@gmail.com>
2023-04-02 16:35:22 -0400

    cambios hasta antes de docker

--- charts/charts.py
+++ charts/charts.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 def generate_pie_chart():
     labels = ['A', 'B', 'C']
-    values = [200,34, 12]
+    values = [200, 34, 12]
     
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
